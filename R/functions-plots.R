2baa7901e4b06305b41f53df65a42de8f0aa855c
Seb Dalgarno <seb@poissonconsulting.ca>
2025-10-17 09:28:28 -0700

    run styler, add decimal.mark

--- R/functions-plots.R
+++ R/functions-plots.R
@@ -1,7 +1,12 @@
 plot_distributions <- function(x, ylab, xlab, text_size) {
-  gp <- ssdtools::ssd_plot_cdf(x,
-    ylab = ylab, xlab = xlab,
-    delta = Inf, average = NA, theme_classic = TRUE, text_size = text_size
+  gp <- ssdtools::ssd_plot_cdf(
+    x,
+    ylab = ylab,
+    xlab = xlab,
+    delta = Inf,
+    average = NA,
+    theme_classic = TRUE,
+    text_size = text_size
   )
   gp
 }
@@ -11,10 +16,31 @@ gp_xbreaks <- function(gp) {
   sort(signif(as.numeric(stats::na.omit(breaks)), 3))
 }
 
-plot_predictions <- function(x, pred, conc, label, colour, shape, percent,
-                             label_adjust, xaxis, yaxis, title, xmin, xmax, palette,
-                             legend_colour, legend_shape, xbreaks = NULL, trans, text_size,
-                             label_size, conc_value, big.mark) {
+plot_predictions <- function(
+  x,
+  pred,
+  conc,
+  label,
+  colour,
+  shape,
+  percent,
+  label_adjust,
+  xaxis,
+  yaxis,
+  title,
+  xmin,
+  xmax,
+  palette,
+  legend_colour,
+  legend_shape,
+  xbreaks = NULL,
+  trans,
+  text_size,
+  label_size,
+  conc_value,
+  big.mark,
+  decimal.mark
+) {
   proportion <- percent / 100
   if (!length(proportion)) {
     proportion <- NULL
@@ -25,13 +51,26 @@ plot_predictions <- function(x, pred, conc, label, colour, shape, percent,
     xlimits <- NULL
   }
 
-  gp <- ssdtools::ssd_plot(x,
+  gp <- ssdtools::ssd_plot(
+    x,
     pred = pred,
-    left = conc, label = label, shape = shape, color = colour,
-    label_size = label_size, xlab = xaxis, ylab = yaxis,
-    ci = FALSE, hc = proportion, shift_x = as.numeric(label_adjust),
-    big.mark = big.mark, trans = trans, xlimits = xlimits,
-    xbreaks = xbreaks, text_size = text_size, theme_classic = TRUE
+    left = conc,
+    label = label,
+    shape = shape,
+    color = colour,
+    label_size = label_size,
+    xlab = xaxis,
+    ylab = yaxis,
+    ci = FALSE,
+    hc = proportion,
+    shift_x = as.numeric(label_adjust),
+    big.mark = big.mark,
+    trans = trans,
+    xlimits = xlimits,
+    xbreaks = xbreaks,
+    text_size = text_size,
+    theme_classic = TRUE,
+    decimal.mark = decimal.mark
   ) +
     ggplot2::scale_color_brewer(palette = palette, name = legend_colour) +
     ggplot2::scale_shape(name = legend_shape) +
